9a86c346c40a01adceb61fe433dd57fe79e7bb51
dakota831 <drtorres831@gmail.com>
2021-11-04 23:47:45 -0400

    20%

--- operators.js
+++ operators.js
@@ -22,3 +22,65 @@ let y = 3;
 // console.log(--x); // decrements x by 1 then prints 9
 // console.log(x--); // prints 10 and then decrements x by 1
 
+//same assignment operator
+x = x + 5;
+x += 5;
+
+//multiply x by 3
+x *= 3;
+x= x * 3;
+
+
+//comparison operators
+
+// Relational operators
+console.log(x>0)
+console.log(x >= 1);
+console.log(x < 1);
+console.log(x <= 1);
+
+//Equality operators
+console.log(x === 1);
+console.log(x !== 1);
+
+//Lose equality (Converts Types/ Only Value)
+console.log( 1 == 1 );
+console.log( 1 == '1' );
+console.log(true == 1);
+
+// Strick equality (same type _and_ value)
+// console.log( 1 === 1 );
+// console.log( 1 === '1' );
+
+
+
+// If a customer has more than 100 points, 
+//they are a 'gold' customer. 
+//Otherwise, they are a 'silver' customer.
+
+let points = 90;
+let type = points > 100 ? 'gold' : 'silver';
+
+console.log(type);
+
+
+// Logical AND (&&)
+// Returns true if both operands are true,
+console.log(true && true); // true
+console.log(true && false); // false
+console.log(false && true); // false
+console.log(false && false); // false
+
+
+//Logical OR (||)
+//Returns true if one of the operands is true.
+
+let highIncome = false;
+let goodCredit = false;
+let elidgibility = highIncome || goodCredit;
+console.log('Eligible', elidgibility);
+
+// Not (!)
+let applicationRefused = !elidgibility;
+
+console.log('Application Refused', applicationRefused;
